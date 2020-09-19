176a53226ee3403e4f59a71e4a4cd79bb281420a
evoituz <evolutionit.uz@gmail.com>
2020-09-19 14:45:21 +0500

    Смена символа $ на Ж

--- spms/urls.py
+++ spms/urls.py
@@ -3,16 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from apps.catalogs.views import *
-from apps.main.views import SettingsViews
+from apps.catalogs import views as catalog_views
+from apps.main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('settings/', SettingsViews.as_view(), name='settings'),
-    path('products/', PriceListViews.as_view(), name='products'),
-    path('product/<int:product_id>/profile/<int:profile_id>/', ProductProfileViews.as_view(), name='product-profile'),
-    path('product/<int:product_id>/profile/add/', ProductProfileAdd.as_view(), name='product-profile-add'),
-    path('product/<int:product_id>/profile/<int:profile_id>/size/add/', add_size_to_profile, name='add-size-to-profile'),
+    path('settings/', main_views.SettingsViews.as_view(), name='settings'),
+    path('products/', catalog_views.PriceListViews.as_view(), name='products'),
+    path('product/<int:product_id>/profile/<int:profile_id>/', catalog_views.ProductProfileViews.as_view(), name='product-profile'),
+    path('product/<int:product_id>/profile/add/', catalog_views.ProductProfileAdd.as_view(), name='product-profile-add'),
+    path('product/<int:product_id>/profile/<int:profile_id>/size/add/', catalog_views.add_size_to_profile, name='add-size-to-profile'),
+    path('', main_views.HomePageViews.as_view(), name='home-page'),
 ]
 
 if settings.DEBUG:
