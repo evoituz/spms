1a2dc0ad30577839f570abfb8e516718f980a51f
evoituz <evolutionit.uz@gmail.com>
2020-10-07 11:30:43 +0500

    модельки для оформления заказа

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField('Имя клиента', max_length=250)
+    balance = models.DecimalField('Баланс', max_digits=25, decimal_places=0, default=0)  # При транзакции, необходимо записывать оплату сюда. Если долг минусовать, если оплатили плюсовать
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, verbose_name='Клиент', on_delete=models.CASCADE)
+
+    created_dt = models.DateTimeField('Дата и время покупки', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def get_total_price(self):
+        return sum([item.price for item in self.items.all()])
+
+    def __str__(self):
+        return self.client.name
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, verbose_name='Заказ', related_name='items', on_delete=models.CASCADE)
+    product_title = models.CharField('Название товара', max_length=250)
+    quantity = models.CharField('Количество', max_length=250)  # Количество и тип количество (4000 гр.)
+    price = models.DecimalField('Общая сумма', max_digits=15, decimal_places=0)
+
+    class Meta:
+        verbose_name = 'Товар заказа'
+        verbose_name_plural = 'Товары заказа'
+
+    def __str__(self):
+        return self.product_title
+
+
+class Transaction(models.Model):
+    DEBT = 'debt'
+    PAYMENT = 'payment'
+    PAID_STATUS = (
+        (DEBT, 'В долг'),
+        (PAYMENT, 'Оплачено'),
+    )
+
+    client = models.ForeignKey(Client, verbose_name='Клиент', on_delete=models.CASCADE, blank=True)  # Не всегда может быть указан клиент, так как покупатель может быть проходцем.
+    order = models.ForeignKey(Order, verbose_name='Заказ', on_delete=models.CASCADE, blank=True)  # Не всегда оплата может производится за заказ, к примеру внесли часть долга
+    amount = models.DecimalField('Сумма', max_digits=15, decimal_places=0)
+    paid = models.CharField('Оплата', choices=PAID_STATUS, max_length=50)  # На случай, если заказ был совершён в долг
+
+    created_dt = models.DateTimeField('Дата и время покупки', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
+
+    def __str__(self):
+        return self.client.name
